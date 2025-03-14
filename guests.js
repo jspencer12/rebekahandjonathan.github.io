4d45f6bcaba6d638bd366a1c4332a688c0afd108
Jonathan Spencer <jollyjonathan2@gmail.com>
2025-03-14 15:04:11 -0700

    Single page skeleton

---
+++ guests.js
@@ -0,0 +1,10 @@
+const parsedGuests = [
+    {"Name": "Jonathan Spencer", "Event": "Idaho"},
+    {"Name": "Rebekah Westerlind", "Event": "Friday"},
+    {"Name": "Charlie Brown", "Event": "Idaho"},
+    {"Name": "Diana Miller", "Event": ""},
+    {"Name": "Edward Davis", "Event": "Friday"},
+    {"Name": "Fiona Green", "Event": "Idaho"},
+    {"Name": "George White", "Event": ""},
+    {"Name": "Heather Gray", "Event": "Friday"}
+];
